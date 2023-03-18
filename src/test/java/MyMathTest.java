666177957b826c0b6689ceb952bd86653522d776
Vuong Dang <vdang1128@gmail.com>
2023-03-18 16:07:47 -0700

    Committing the project files

---
+++ src/test/java/MyMathTest.java
@@ -0,0 +1,25 @@
+import org.example.math.MyMath;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MyMathTest
+{
+
+    @Test
+    public void exceptionsTest()
+    {
+        MyMath calculator = new MyMath();
+        assertThrows(ArithmeticException.class, () -> calculator.divide(219, 0));
+    }
+
+    @Test
+    public void divideTest()
+    {
+        MyMath calculator = new MyMath();
+        assertDoesNotThrow(() -> calculator.divide(219, 21));
+
+    }
+
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,19 @@
+import org.example.people.Person;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonTest
+{
+    @Test
+    public void constructorTest()
+    {
+        Person testMe = new Person("Joe", "Smith");
+
+        assertAll ("The constructor does not set all fields correctly",
+                () -> assertEquals("Joe", testMe.getFirst()),
+                () -> assertEquals("Smith", testMe.getLast()),
+                () -> assertNull(testMe.getTitle())
+        );
+    }
+}
